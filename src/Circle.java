341c5a0ba066e65bc0b130ed6b841984ff736592
unknown <BT_3W2_11@BKTECH.LOCAL>
2022-10-17 12:33:20 -0400

    Initial commit

---
+++ src/Circle.java
@@ -0,0 +1,22 @@
+public class Circle {
+    private double radius;
+
+    public Circle(double radius)
+    {
+        this.radius = radius;
+    }
+
+    public double area()
+    { return Math.PI*Math.pow(radius,2); }
+
+    public double circumference()
+    { return Math.PI*2*radius; }
+
+    public void setRadius(double newRadius)
+    { this.radius = newRadius; }
+
+    public String getInfo() {
+        return "radius: " + radius + "\narea: " + area() + "\ncircumference: " + circumference();
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print(Math.random()*9+1);
+    }
+}
